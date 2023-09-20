f85d9b964d8781519bc455aa942b2568b1f15169
GoingCrazyDude <tezosbiswa@gmail.com>
2023-09-21 01:24:58 +0530

    fixed

---
+++ Patch.ps1
@@ -0,0 +1,9 @@
+$rblxPath = Get-ItemPropertyValue -Path 'Registry::HKEY_CURRENT_USER\Software\ROBLOX Corporation\Environments\roblox-player' -Name '(Default)'
+$rblxPath = $rblxpath -replace 'RobloxPlayerLauncher.exe'
+$rblxAltPath = Get-ItemPropertyValue -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\roblox-player-admin' -Name 'InstallLocation'
+
+try {
+    iwr -uri 'https://raw.githubusercontent.com/rbxflags/Flags/main/ClientAppSettings.json' -OutFile(New-Item -Path $rblxPath/ClientSettings/ClientAppSettings.json -Force)
+} catch {
+    iwr -uri 'https://raw.githubusercontent.com/rbxflags/Flags/main/ClientAppSettings.json' -OutFile(New-Item -Path $rblxAltPath/ClientSettings/ClientAppSettings.json -Force)
+}

b4a15691c97d6a682aa1506134714fc493e72ba3
GoingCrazyDude <tezosbiswa@gmail.com>
2023-09-25 00:46:10 +0530

    Test

--- Patch.ps1
+++ Patch.ps1
@@ -1,10 +1,21 @@
-if(!([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
-    Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList "-File `"$($MyInvocation.MyCommand.Path)`"  `"$($MyInvocation.MyCommand.UnboundArguments)`""
-    Exit
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+        $Command = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+        Start-Process -FilePath PowerShell.exe -Verb RunAs -ArgumentList $Command
+        Exit
+ }
 }
 
-$rblxPath = Get-ItemPropertyValue -Path 'Registry::HKEY_CURRENT_USER\Software\ROBLOX Corporation\Environments\roblox-player' -Name '(Default)'
-$rblxPath = $rblxpath -replace 'RobloxPlayerLauncher.exe'
-$rblxAltPath = Get-ItemPropertyValue -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\roblox-player-admin' -Name 'InstallLocation'
-iwr -uri 'https://raw.githubusercontent.com/rbxflags/Flags/main/ClientAppSettings.json' -OutFile(New-Item -Path $rblxPath/ClientSettings/ClientAppSettings.json -Force)
-iwr -uri 'https://raw.githubusercontent.com/rbxflags/Flags/main/ClientAppSettings.json' -OutFile(New-Item -Path $rblxAltPath/ClientSettings/ClientAppSettings.json -Force)
+try {
+    $rblxPath = Get-ItemPropertyValue -LiteralPath 'HKCU:\Software\ROBLOX Corporation\Environments\roblox-player' -Name '(Default)'
+    $rblxPath = $rblxpath -replace 'RobloxPlayerLauncher.exe'
+    echo $rblxPath
+    iwr -uri 'https://raw.githubusercontent.com/rbxflags/Flags/main/ClientAppSettings.json' -OutFile(New-Item -Path $rblxPath/ClientSettings/ClientAppSettings.json -Force)
+    echo 'Installed Successfully'
+}
+catch {
+    $rblxAltPath = Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\roblox-player-admin' -Name 'InstallLocation'
+    echo $rblxAltPath
+    iwr -uri 'https://raw.githubusercontent.com/rbxflags/Flags/main/ClientAppSettings.json' -OutFile(New-Item -Path $rblxAltPath/ClientSettings/ClientAppSettings.json -Force)
+    echo 'Installed Successfully'
+}
